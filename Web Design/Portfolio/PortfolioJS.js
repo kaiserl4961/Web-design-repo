c406ab4b5f85c9b4458dc1511161915068c84a10
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2022-05-23 09:11:19 -0500

    updated stuff

--- Web Design/Portfolio/PortfolioJS.js
+++ Web Design/Portfolio/PortfolioJS.js
@@ -63,3 +63,15 @@ function ball() {
         setInterval(moveBall, 10);
     })
 };
+const rickroll = function(event) {
+    event.preventDefault();
+    document.write("Get 'rolled lol");
+    window.setTimeout(function() {
+        document.body.innerHTML =
+            '<iframe width="966" height="543" src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1" frameborder="0"></iframe>';
+        console.info("https://scratch.mit.edu/discuss/post/5069102/");
+    }, 500);
+};
+document
+    .querySelectorAll(".rickroll")
+    .forEach((el) => el.addEventListener("click", rickroll));
