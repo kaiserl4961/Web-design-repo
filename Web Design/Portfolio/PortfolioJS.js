6aa2e8f52f012685eccea86264cbcc5e7e4f23f6
Targed <ltklionel@gmail.com>
2022-05-29 14:20:35 -0500

    did stuff

--- Web Design/Portfolio/PortfolioJS.js
+++ Web Design/Portfolio/PortfolioJS.js
@@ -75,3 +75,49 @@ const rickroll = function(event) {
 document
     .querySelectorAll(".rickroll")
     .forEach((el) => el.addEventListener("click", rickroll));
+
+function scrollStickyElement() {
+    var stickyElement = document.querySelector(".sticky");
+    var stickyElementOffset = stickyElement.offsetTop;
+    var stickyElementHeight = stickyElement.offsetHeight;
+
+    window.addEventListener("scroll", function() {
+        var scrollTop = window.pageYOffset;
+        var stickyElementCurrentPosition = stickyElementOffset - scrollTop;
+        var stickyElementNewPosition = stickyElementHeight + stickyElementCurrentPosition;
+        stickyElement.style.top = stickyElementNewPosition + "px";
+    });
+}
+
+function scrollElement() {
+    var element = document.querySelector(".scroll");
+    var elementOffset = element.offsetTop;
+    var elementHeight = element.offsetHeight;
+
+    window.addEventListener("scroll", function() {
+        var scrollTop = window.pageYOffset;
+        var elementCurrentPosition = elementOffset - scrollTop;
+        var elementNewPosition = elementHeight + elementCurrentPosition;
+        element.style.top = elementNewPosition + "px";
+    });
+}
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+/*var button = document.getElementById('button'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('myDiv');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+};*/
